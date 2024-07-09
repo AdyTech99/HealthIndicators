cb79c0db744b1c08b0069fe62900c5928f824a62
AdyTech99 <113180531+AdyTech99@users.noreply.github.com>
2024-07-09 13:18:17 +0530

    why is there an extra "src"

--- src/main/java/io/github/adytech99/healthindicators/util/Maths.java
+++
@@ -1,7 +0,0 @@
-package io.github.adytech99.healthindicators.util;
-
-public class Maths {
-    public static double truncate(double number, int places) {
-        return Math.floor(number * Math.pow(10, places)) / Math.pow(10, places);
-    }
-}
