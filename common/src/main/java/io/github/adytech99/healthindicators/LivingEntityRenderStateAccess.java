1a1583bbc712530e1edd7837d66f8e2d20169e38
AdyTech99 <snackwack99@gmail.com>
2024-10-23 20:42:17 +0530

    chore: delete unused mixins/accessors

--- common/src/main/java/io/github/adytech99/healthindicators/LivingEntityRenderStateAccess.java
+++
@@ -1,8 +0,0 @@
-package io.github.adytech99.healthindicators;
-
-import net.minecraft.entity.LivingEntity;
-
-public interface LivingEntityRenderStateAccess {
-    LivingEntity healthindicators$getLivingEntity();
-    void healthindicators$setLivingEntity(LivingEntity livingEntity);
-}
